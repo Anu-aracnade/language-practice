660637006a1a58b128c51edb981fefff14ffb165
Anu-aracnade <anubhab01.aracnade@gmail.com>
2025-10-05 23:49:22 +0530

    feat: add a poem in Python using multiple line print

---
+++ python-basics/printmultiplelineProgram_1.py
@@ -0,0 +1,30 @@
+print("Hello !, Your poem in python😁\n")
+print("""What is poetry?
+that pair of eyes- murky, dreamy,
+the kind you wish would engulf you whole
+and you would not flinch, for it is him.
+That voice- warm like honey in the cold,
+soothing my soul without me
+needing a single utterance
+that is poetry.
+The way he says my name,
+like it holds all the weight in the world
+the way he listens, really listens
+like every word of mine deserves a home,
+the way he looks out for me,
+like I am a ten year old all over again
+that is poetry.
+Amidst the crowd, my eyes find his,
+my fidgety pair meeting his steady ones
+and he smiles- that reassuring smile,
+the kind that could stitch
+your wretched soul back together,
+and paint your somber, black world
+into every hopeful shade of gold
+that is poetry.
+What is poetry?
+his care, his silence, him.
+To me, he is a poem, personified.
+I could write him for eternity,
+thought trailing thought, line upon line,
+and call that poetry.""")
