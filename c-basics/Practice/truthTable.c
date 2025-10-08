4e9caa03b81e8c4a8619c277d7cfc2acc7ef6954
Anu-aracnade <anubhab01.aracnade@gmail.com>
2025-10-08 13:48:54 +0530

    feat : add program to generate truth table using \t

---
+++ c-basics/Practice/truthTable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("A\tB\tA && B\n");
+    for (a = 0; a <= 1; a++) {
+        for (b = 0; b <= 1; b++) {
+            printf("%d\t%d\t%d\n", a, b, a && b);
+        }
+    }
+    return 0;
+}
+// here the main thing is the use '\t' to add even gaps between columns or 'tab space'.
