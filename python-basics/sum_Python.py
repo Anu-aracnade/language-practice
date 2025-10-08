4a36951c2d48d7610359f02ddd6672b77ded115a
Anu-aracnade <anubhab01.aracnade@gmail.com>
2025-10-08 13:39:20 +0530

    feat: add basic arithmetic and logical operations in Python

---
+++ python-basics/sum_Python.py
@@ -0,0 +1,19 @@
+a = 100
+b = 50
+print("Hey your answer is", a + b)
+
+a = 30
+a += 5  # Assignment operators
+print(a)  # first increment the value of 'a' by 5 and then assign it to 'a'
+b = 45
+b -= 6
+print(b)  # first decrement the value of 'b' by 6 and then assign it to 'b'
+ # Comparision operator
+c = 6 <= 6  # meaning -> less then or equal to 6
+print(c)
+
+# Logical operator
+
+print("Value of True and False is ", True and False)  # Ouput will be False
+print("Value of True or False is ", True or False)  # Ouput will be True
+print("Value of not True is ", not(True))        # Output will be False  
