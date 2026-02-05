88161ca7c19cc288a30dbd2010ecb39962a8a1b3
Anu-aracnade <anubhab01.aracnade@gmail.com>
2026-02-05 22:14:18 +0530

    feat: add cpp program for first occurence in a string

---
+++ cpp-basics/leetcode problems/firstOccurence.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+int strStr(string haystack , string needle){
+    int n =haystack.length();
+    int m = needle.length();
+    if (n==0)
+    return 0;
+    if (n<m)
+    return -1;
+    for (int i =0; i<=n-m; i++){
+        int j =0; // the pointer that works for needle indexing 
+        while (j<m && haystack[i+j]==needle[j]){
+            j++;
+        }
+        if (j==m){
+            return i;
+        }
+    }
+    return -1;
+}
+}str1,str2;
+
+int main(){
+    string a = "Anubhab";
+    string b = "Anu";
+    int result = str1.strStr(a,b);
+    cout<< "Index: "<<result<<endl;
+    return 0;
+}
