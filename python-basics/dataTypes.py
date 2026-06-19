3e63555b913dbad4d66500bace62a75c113972d2
Anu-aracnade <anubhab01.aracnade@gmail.com>
2026-06-19 17:30:05 +0530

    feat: Add different datatypes with their types

---
+++ python-basics/dataTypes.py
@@ -0,0 +1,29 @@
+name = "Anubhab"
+print(name,type(name))
+
+age = 20
+print(age,type(age))
+
+height = 5.10
+print(height,type(height))
+
+set = {"Baido",25,4.11}
+print(set,type(set))
+
+dictio = {'Mon': 20,'Baido': 25}
+print(dictio,type(dictio))
+
+tuple = ('Mon',20,5.10)
+print(tuple,type(tuple))
+
+boolean = True
+print(boolean,type(boolean))
+
+Range = range(6)
+print(Range,type(Range))
+
+list = ['Mon',20,'Baido',25,True]
+print(list,type(list))
+
+my_none_var = None
+print(my_none_var,type(my_none_var))
