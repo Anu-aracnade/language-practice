89e7f8390d7bbb62bc268715b40d6c6aa6a11f8a
Anu-aracnade <anubhab01.aracnade@gmail.com>
2026-06-27 14:15:27 +0530

    feat:Add program to verify divisibility with 3 and 5

---
+++ java-script-basics/fizzBuzz.js
@@ -0,0 +1,11 @@
+function fizzBuzz(num1) {
+    if (num1 % 5 === 0 && num1 % 3 === 0)
+        return "FizzBuzz";
+    else if (num1 % 3 === 0)
+        return "Fizz";
+    else if (num1 % 5 === 0)
+        return "Buzz";
+    else
+        return num1;
+}
+console.log(fizzBuzz(30));
