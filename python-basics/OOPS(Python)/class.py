dae7cc6103744e739073821dcc683d43c6d47fb0
Anu-aracnade <anubhab01.aracnade@gmail.com>
2026-07-16 23:26:11 +0530

    feat:Add object name with class attribute

--- python-basics/OOPS(Python)/class.py
+++ python-basics/OOPS(Python)/class.py
@@ -5,3 +5,8 @@ class Dog:
     def __init__(self, name, age):
         self.name = name  # Instance attribute
         self.age = age  # Instance attribute
+
+# Creating an object of the Dog class
+dog1 = Dog("Buddy", 3)
+print(dog1.name) 
+print(dog1.species)
