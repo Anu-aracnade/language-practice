8a2fb599e2fb542d4dba29a1956f3dcc6eedbe1c
Anu-aracnade <anubhab01.aracnade@gmail.com>
2026-08-01 23:36:36 +0530

    feat: add program for permutation of array in cpp using vector

---
+++ cpp-basics/leetcode problems/permutationArray.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            nums[i] = nums[i] + (nums[nums[i]] % n) * n;
+        }
+        for (int i = 0; i < n; ++i) {
+            nums[i] /= n;
+        }
+        return nums;
+    }
+};
